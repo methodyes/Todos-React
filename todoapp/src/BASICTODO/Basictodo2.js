474988730da1da6ff24261095bea2fa3c12a1a67
metho <metholk@gmail.com>
2019-02-22 17:59:21 +0530

    added if condtion to prevent blank task adding to the array

--- todoapp/src/BASICTODO/Basictodo2.js
+++ todoapp/src/BASICTODO/Basictodo2.js
@@ -1,64 +1,61 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
- class Basictodo2 extends Component {
-     constructor(props) {
-       super(props)
-     
-       this.state = {
-          title : 'Map arrray when press the enter button ',
-          input : '',
-          list :[]
-       }
-     }
-     changeHandler=(t)=>{
-         this.setState({
-             input : t.target.value
-         })
-     }
-     pressEnter=(e)=>{
-        //  var l = this.state.list;
-         var i= this.state.input;
-      if(e.which===13){
-    //    l.push(i)
-    this.state.list.push(i)
-       this.setState({
+class Basictodo2 extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      title: "Map arrray when press the enter button ",
+      input: "",
+      list: []
+    };
+  }
+  changeHandler = t => {
+    this.setState({
+      input: t.target.value
+    });
+  };
+
+  pressEnter = e => {
+    //  var l = this.state.list;
+    var i = this.state.input;
+
+    if (e.which === 13) {
+      //    l.push(i)
+      if(i){
+      
+        // const list = [...this.state.list, i];
+      
+      this.state.list.push(i);
+      this.setState({
         //    list : l,
-        list : this.state.list,
-           input : '',
-          
+        list: this.state.list,
+        input: ""
+      })}
+      e.preventDefault();
+    }
+  };
 
-       })
-       e.preventDefault()
-      }
-     }
-     
   render() {
-      var {title,input,list}=this.state
+    var { title, input, list } = this.state;
     return (
       <div>
+        <form>
           <h2>{title}</h2>
-        
-            <input
-            onKeyDown={this.pressEnter}
-            type="text"
-            value={input}
-            onChange={this.changeHandler}
-            />
-     
-         <div>
-        <ol>
-            {list.map((data)=>{
-                return (
-                    <li>{data}</li>
-                )
-            })}
-        </ol>
-        </div>
-      
-        
+
+          <input onKeyDown={this.pressEnter} type="text" value={input} onChange={this.changeHandler} />
+
+          <div>
+            <ol>
+              {list.map(data => {
+                return <li>{data}</li>;
+              })}
+            </ol>
+          </div>
+        </form>
       </div>
-    )
+    );
   }
 }
 
-export default Basictodo2
+export default Basictodo2;
